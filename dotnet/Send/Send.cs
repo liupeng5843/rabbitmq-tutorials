58bacd320d7872d7ea2dbc98495780fe451ba96d
archer <584314285@qq.com>
2017-10-30 18:20:54 +0800

    update send.cs
    i think it requires {}.

--- dotnet/Send/Send.cs
+++ dotnet/Send/Send.cs
@@ -8,6 +8,7 @@ class Send
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
         using(var connection = factory.CreateConnection())
+        {
         using(var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
@@ -17,6 +18,7 @@ class Send
 
             channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
             Console.WriteLine(" [x] Sent {0}", message);
+        }
         }
 
         Console.WriteLine(" Press [enter] to exit.");
